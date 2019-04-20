7d62de37ce7b5e819a45ec5f7bff5a7fa7722a29
zijianchen <chenbilly97@gmail.com>
2019-04-20 19:08:17 -0400

    two sum

---
+++ 1_two_sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int len = nums.length;
+        HashMap<Integer,Integer> map = new HashMap();
+        for (int i=0;i<len;i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{i,map.get(complement)};
+            }
+            map.put(nums[i],i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
+}
