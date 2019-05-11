30806bef9492ea1f3bebfb37ab24160c59845ae5
zijianchen <chenbilly97@gmail.com>
2019-05-11 17:48:54 -0400

    3  sum

--- 15_3Sum.java
+++ 15_3Sum.java
@@ -1,24 +1,23 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<List<Integer>>();;
-        if (nums == null || nums.length ==0) {
-            return res;
+        if (nums == null || nums.length == 0) {
+            return result;
         }
         Arrays.sort(nums);
         int len = nums.length;
         for (int i=0;i<len;i++) {
-            while (i>0 && nums[i]==nums[i-1]) continue;
+            if (i>0 && nums[i]==nums[i-1]) continue;
             int l = i+1, r = len-1;
             while (l < r) {
-                System.out.println(i+","+l+","+r);
                 if (nums[i]+nums[l]+nums[r]==0) {
                     List<Integer> list = new ArrayList<Integer>();
                     list.add(nums[i]);
                     list.add(nums[l]);
                     list.add(nums[r]);
                     result.add(list);
-                    while ( l<r && nums[l]==nums[l-1]) l++;
-                    while ( l<r && nums[r]==nums[r+1]) r--;
+                    while ( l<r && nums[l]==nums[l+1]) l++;
+                    while ( l<r && nums[r]==nums[r-1]) r--;
                     l++;
                     r--;
                 }
@@ -33,3 +32,4 @@ class Solution {
         return result;
     }
 }
+
